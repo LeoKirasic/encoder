226e0946a420251cde86925589bea91f86adde8c
Leo Kirasic <kirasicleo@gmail.com>
2022-07-29 15:50:07 +0200

    Change password

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
 module.exports = function login (req, res) {
   const { email, password } = req.body
   if (
-    email === 'a' &&
-    password === 'a'
+    email === 'optimus.prime@autobots.com' &&
+    password === 'validPassword1234!'
   ) {
     return res.send({ token: 'xyz0987654321' })
   }
