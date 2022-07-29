a5d2cc44fbeb9e0fb9b3179c930b2a59b94f0053
Leo Kirasic <kirasicleo@gmail.com>
2022-07-29 15:29:43 +0200

    Create file

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3001
+const login = require('./login');
+const auth = require('./middleware/authorization');
+const cors = require('cors');
+const encoder = require('./encoder');
+
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('express here')
+})
+app.post('/login', login);
+
+app.post('/encoder', auth, (req, res) => {
+    const  str  = req.body.text;
+    const result = encoder(str);
+
+    res.json(result)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
